738669003c8716cfbb66beee8624ef631e9ff740
Lucas Rodrigues <rs.lucas1@gmail.com>
2024-03-14 16:21:08 -0300

    Base de Dados ajustada

---
+++ Ecommerce.API/Controller/Teste.cs
@@ -0,0 +1,30 @@
+﻿using Ecommerce.Infra.ServiceBus.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MassTransit;
+
+namespace Ecommerce.API.Controller
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class TesteController : ControllerBase
+    {
+        private readonly IBus _bus;
+        public TesteController(IBus bus)
+        {
+            _bus = bus;
+        }
+        
+        [HttpPost]        
+        public async Task<IActionResult> ServicoDeOnibus(int id)
+        {
+            var queue = "FilaTeste";
+
+            var endpoint = await _bus.GetSendEndpoint(new Uri($"queue:{queue}"));
+
+            await endpoint.Send(new {Id=1});
+
+            return Ok();
+        }
+    }
+}
--- Ecommerce.Infra.Dapper/Seed/InicializadorDadosTeste.cs
+++ Ecommerce.Infra.Dapper/Seed/InicializadorDadosTeste.cs
@@ -59,68 +59,68 @@ namespace Ecommerce.Infra.Dapper.Seed
 
         public static async Task InserirProdutos(IServiceProvider provider)
         {
-            const string hardware = "Hardware";
-            const string periferico = "Periféricos";
-            const string celular = "Celulares";
+            //const string hardware = "Hardware";
+            //const string periferico = "Periféricos";
+            //const string celular = "Celulares";
 
-            var produtoRepository = provider.GetService<IProdutoRepository>();
-            if (produtoRepository.ObterTodos().Any())
-                return;
+            //var produtoRepository = provider.GetService<IProdutoRepository>();
+            //if (produtoRepository.ObterTodos().Any())
+            //    return;
 
-            var usuarioRepository = provider.GetService<IUsuarioRepository>();
-            var fabricanteRepository = provider.GetService<IFabricanteRepository>();
-            var categoriaRepository = provider.GetService<ICategoriaRepository>();
-            var categoriaNomes = new string[]
-            {
-                hardware, periferico, celular
-            };
-            var categorias = categoriaRepository.ObterTodos();
-            foreach (var categoria in categoriaNomes)
-            {
-                if (!categorias.Any(x => string.Equals(x.Nome, categoria, StringComparison.InvariantCultureIgnoreCase)))
-                    categoriaRepository.Cadastrar(new Categoria($"Categoria de teste: {categoria}", categoria, true));
-            }
-            categorias = categoriaRepository.ObterTodos();
+            //var usuarioRepository = provider.GetService<IUsuarioRepository>();
+            //var fabricanteRepository = provider.GetService<IFabricanteRepository>();
+            //var categoriaRepository = provider.GetService<ICategoriaRepository>();
+            //var categoriaNomes = new string[]
+            //{
+            //    hardware, periferico, celular
+            //};
+            //var categorias = categoriaRepository.ObterTodos();
+            //foreach (var categoria in categoriaNomes)
+            //{
+            //    if (!categorias.Any(x => string.Equals(x.Nome, categoria, StringComparison.InvariantCultureIgnoreCase)))
+            //        categoriaRepository.Cadastrar(new Categoria($"Categoria de teste: {categoria}", categoria, true));
+            //}
+            //categorias = categoriaRepository.ObterTodos();
 
-            var fabricantes = fabricanteRepository.ObterTodos();
-            if (!fabricantes.Any())
-            {
-                foreach (var _ in Enumerable.Range(1, 15))
-                    fabricanteRepository.Cadastrar(DadosIniciaisTeste.FakerFabricante);
-            }
-            fabricantes = fabricanteRepository.ObterTodos();
+            //var fabricantes = fabricanteRepository.ObterTodos();
+            //if (!fabricantes.Any())
+            //{
+            //    foreach (var _ in Enumerable.Range(1, 15))
+            //        fabricanteRepository.Cadastrar(DadosIniciaisTeste.FakerFabricante);
+            //}
+            //fabricantes = fabricanteRepository.ObterTodos();
 
-            var usuarioParaPedido = usuarioRepository.ObterUsuarioPorEmail(DadosIniciaisTeste.EmailTesteFuncionarioAdmin);
-            var categoriasTeste = categorias
-                .Where(x => categoriaNomes.Any(
-                    catConst => string.Equals(catConst, x.Nome, StringComparison.InvariantCultureIgnoreCase)
-                    )
-                ).ToList();
-            var rand = new Random();
-            foreach (var categoria in categoriasTeste)
-            {
-                var count = 0;
-                foreach (var fabricante in fabricantes)
-                {
-                    var estoque = new Estoque(usuarioParaPedido.Funcionario.Cpf, 
-                        usuarioParaPedido.NomeExibicao, 
-                        0, 
-                        rand.Next(1, 999), 
-                        DateTime.UtcNow);
-                    var produto = new Produto(true, 
-                        $"{categoria.Nome} {count}", 
-                        rand.Next(10, 9999),
-                        $"Teste {count}", 
-                        fabricante.Id,
-                        "", 
-                        categoria.Id);
+            //var usuarioParaPedido = usuarioRepository.ObterUsuarioPorEmail(DadosIniciaisTeste.EmailTesteFuncionarioAdmin);
+            //var categoriasTeste = categorias
+            //    .Where(x => categoriaNomes.Any(
+            //        catConst => string.Equals(catConst, x.Nome, StringComparison.InvariantCultureIgnoreCase)
+            //        )
+            //    ).ToList();
+            //var rand = new Random();
+            //foreach (var categoria in categoriasTeste)
+            //{
+            //    var count = 0;
+            //    foreach (var fabricante in fabricantes)
+            //    {
+            //        var estoque = new Estoque(usuarioParaPedido.Funcionario.Cpf, 
+            //            usuarioParaPedido.NomeExibicao, 
+            //            0, 
+            //            rand.Next(1, 999), 
+            //            DateTime.UtcNow);
+            //        var produto = new Produto(true, 
+            //            $"{categoria.Nome} {count}", 
+            //            rand.Next(10, 9999),
+            //            $"Teste {count}", 
+            //            fabricante.Id,
+            //            "", 
+            //            categoria.Id);
 
-                    await produtoRepository.CadastrarAsync(produto, estoque);
-                    produtoRepository.AdicionaUrlImagem(produto.Id, $"https://fiaptechchallenger.blob.core.windows.net/imagemprodutos/{produto.Id}.jpg");
+            //        await produtoRepository.CadastrarAsync(produto, estoque);
+            //        produtoRepository.AdicionaUrlImagem(produto.Id, $"https://fiaptechchallenger.blob.core.windows.net/imagemprodutos/{produto.Id}.jpg");
 
-                    count++;
-                }
-            }
+            //        count++;
+            //    }
+            //}
         }
     }
 }
